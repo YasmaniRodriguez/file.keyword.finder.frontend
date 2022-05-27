4472cec51b9e3768930e0ac7dd8ca6a3e07c4048
Rodriguez Yasmani <rodriguez.yasmani@hotmail.com>
2022-05-27 11:25:01 -0300

    ajustes varios

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import StyledFooter from "./StyledFooter";
+import { Box, Typography } from "@mui/material";
 
 interface Props {}
 const Main = (props: Props) => {
-  return <StyledFooter></StyledFooter>;
+  return (
+    <StyledFooter>
+      <Box sx={{ margin: "auto" }}>
+        <Typography>© 2022 Copyright Alliance</Typography>
+      </Box>
+    </StyledFooter>
+  );
 };
 
 export default Main;
--- src/components/Footer/StyledFooter.ts
+++ src/components/Footer/StyledFooter.ts
@@ -4,6 +4,7 @@ import { theme } from "../../assets/themes";
 const StyledFooter = styled.footer`
   min-height: 15vh;
   border-top: solid 1px ${theme.colors.primary};
+  display: flex;
 `;
 
 export default StyledFooter;
