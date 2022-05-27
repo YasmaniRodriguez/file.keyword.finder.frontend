6bea94453419e02cb7746be8f8406453febc5052
Rodriguez Yasmani <rodriguez.yasmani@hotmail.com>
2022-05-27 12:28:06 -0300

    ajustes varios

--- src/components/DragDrop/StyledDragDrop.ts
+++ src/components/DragDrop/StyledDragDrop.ts
@@ -2,13 +2,22 @@ import styled from "@emotion/styled";
 import { Dashboard } from "@uppy/react";
 
 const StyledDragDrop = styled(Dashboard)`
-  & .uppy-Container {
-    & .uppy-Root {
-      & .uppy-Dashboard {
-        & .uppy-Dashboard-inner {
-          & .uppy-Dashboard-innerWrap {
-            & .uppy-Dashboard-AddFiles {
-            }
+  margin: auto;
+  width: 50%;
+  height: 50%;
+  position: relative;
+  & .uppy-Root {
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    & .uppy-Dashboard {
+      height: 100%;
+      width: 100%;
+      & .uppy-Dashboard-inner {
+        & .uppy-Dashboard-innerWrap {
+          & .uppy-Dashboard-AddFiles {
           }
         }
       }
--- src/components/UploadWrapper/StyledUploadWrapper.ts
+++ src/components/UploadWrapper/StyledUploadWrapper.ts
@@ -4,7 +4,6 @@ import { Box } from "@mui/material";
 const StyledUploadWrapper = styled(Box)`
   height: 80vh;
   display: flex;
-  border: solid 1px red;
 `;
 
 export default StyledUploadWrapper;
