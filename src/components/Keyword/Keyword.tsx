21139011a290e937fe430a204c20bfd503e710d9
Rodriguez Yasmani <rodriguez.yasmani@hotmail.com>
2022-06-05 23:15:06 -0300

    ajustes varios

--- src/components/Keyword/Keyword.tsx
+++ src/components/Keyword/Keyword.tsx
@@ -1,64 +1,21 @@
-import React, {
-  useContext,
-  ChangeEvent,
-  useState,
-  KeyboardEvent,
-  MouseEvent,
-} from "react";
+import React, { ChangeEvent, KeyboardEvent, MouseEvent } from "react";
 import { Box, Typography, Button, TextField } from "@mui/material";
 import { Settings as SettingsIcon } from "@mui/icons-material";
-import { DialogContext } from "../../contexts/DialogContext";
-import { SnackBarContext } from "../../contexts/SnackBarContext";
-import { AppContext } from "../../contexts/AppContext";
 import SelectWidget from "../../widgets/SelectWidget/SelectWidget";
 import StyledKeyword from "./StyledKeyword";
 import Category from "../Category/Category";
 
-interface Props {}
+interface Props {
+  keyword?: string;
+  changeKeyword: (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => void;
+  handleKeyDown: (event: KeyboardEvent<HTMLDivElement>) => void;
+  handleClick: (event: MouseEvent<HTMLButtonElement>) => void;
+}
 
 const Keyword = (props: Props) => {
-  const dlgCtx = useContext(DialogContext);
-  const appCtx = useContext(AppContext);
-  const snbCtx = useContext(SnackBarContext);
-
-  const [keyword, setKeyword] = useState<string | undefined>();
-
-  const changeKeyword = (
-    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) => {
-    let value = (event.currentTarget as HTMLInputElement).value;
-    setKeyword(value);
-  };
-
-  const handleKeyDown = (event: KeyboardEvent<HTMLDivElement>) => {
-    if (event.key === "Enter" && keyword !== undefined) {
-      let exists = appCtx?.order?.keywords.includes(keyword);
-      if (exists) {
-        snbCtx?.changeMessage(`keyword ${keyword} already exist in the list`);
-        snbCtx?.handleOpen();
-      }
-      return keyword.length > 0
-        ? (appCtx?.addOrderKeyword(keyword), setKeyword(""))
-        : false;
-    }
-  };
-
-  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
-    let action = (event.currentTarget as HTMLButtonElement).ariaLabel;
-
-    switch (action) {
-      case "template-settings":
-        dlgCtx?.handleOpen();
-        break;
-
-      case "order-submit":
-        break;
-
-      default:
-        break;
-    }
-  };
-
+  const { keyword, changeKeyword, handleKeyDown, handleClick } = props;
   return (
     <StyledKeyword>
       <Typography className="order-title" variant="h5" component={"h5"}>
--- src/components/Order/Order.tsx
+++ src/components/Order/Order.tsx
@@ -1,15 +1,71 @@
-import React from "react";
+import React, {
+  useContext,
+  ChangeEvent,
+  useState,
+  KeyboardEvent,
+  MouseEvent,
+} from "react";
 import StyledOrder from "./StyledOrder";
 import Record from "../Record/Record";
 import Keyword from "../Keyword/Keyword";
+import { DialogContext } from "../../contexts/DialogContext";
+import { SnackBarContext } from "../../contexts/SnackBarContext";
+import { AppContext } from "../../contexts/AppContext";
 
 interface Props {}
 
 const Order = (props: Props) => {
+  const dlgCtx = useContext(DialogContext);
+  const appCtx = useContext(AppContext);
+  const snbCtx = useContext(SnackBarContext);
+
+  const [keyword, setKeyword] = useState<string | undefined>();
+
+  const changeKeyword = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    let value = (event.currentTarget as HTMLInputElement).value;
+    setKeyword(value);
+  };
+
+  const handleKeyDown = (event: KeyboardEvent<HTMLDivElement>) => {
+    if (event.key === "Enter" && keyword !== undefined) {
+      let exists = appCtx?.order?.keywords.includes(keyword);
+      if (exists) {
+        snbCtx?.changeMessage(`keyword ${keyword} already exist in the list`);
+        snbCtx?.handleOpen();
+      }
+      return keyword.length > 0
+        ? (appCtx?.addOrderKeyword(keyword), setKeyword(""))
+        : false;
+    }
+  };
+
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    let action = (event.currentTarget as HTMLButtonElement).ariaLabel;
+
+    switch (action) {
+      case "template-settings":
+        dlgCtx?.handleOpen();
+        break;
+
+      case "order-submit":
+        break;
+
+      default:
+        break;
+    }
+  };
+
   return (
     <StyledOrder>
       <Record />
-      <Keyword />
+      <Keyword
+        keyword={keyword}
+        changeKeyword={changeKeyword}
+        handleKeyDown={handleKeyDown}
+        handleClick={handleClick}
+      />
     </StyledOrder>
   );
 };
