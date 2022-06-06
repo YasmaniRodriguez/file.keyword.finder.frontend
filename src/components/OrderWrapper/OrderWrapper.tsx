85149d24e1f24f1b3a06983a04c53bcef66b66ae
Rodriguez Yasmani <rodriguez.yasmani@hotmail.com>
2022-06-06 00:35:46 -0300

    ajustes varios

--- src/components/OrderWrapper/OrderWrapper.tsx
+++ src/components/OrderWrapper/OrderWrapper.tsx
@@ -1,11 +1,88 @@
-import React from "react";
-import Order from "../Order/Order";
+import React, {
+  useContext,
+  ChangeEvent,
+  useState,
+  KeyboardEvent,
+  MouseEvent,
+} from "react";
 import StyledOrderWrapper from "./StyledOrderWrapper";
+import { theme } from "../../assets/themes";
+import Record from "../Record/Record";
+import Search from "../Search/Search";
+import Template from "../Template/Template";
+import { DialogContext } from "../../contexts/DialogContext";
+import { SnackBarContext } from "../../contexts/SnackBarContext";
+import { AppContext } from "../../contexts/AppContext";
+import { Box } from "@mui/material";
 
-const OrderWrapper = () => {
+interface Props {}
+
+const OrderWrapper = (props: Props) => {
+  const dlgCtx = useContext(DialogContext);
+  const appCtx = useContext(AppContext);
+  const snbCtx = useContext(SnackBarContext);
+
+  const [keyword, setKeyword] = useState<string | undefined>();
+
+  const changeKeyword = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    let value = (event.currentTarget as HTMLInputElement).value;
+    setKeyword(value);
+  };
+
+  const handleKeyDown = (event: KeyboardEvent<HTMLDivElement>) => {
+    if (event.key === "Enter" && keyword !== undefined) {
+      let exists = appCtx?.order?.keywords.includes(keyword);
+      if (exists) {
+        snbCtx?.changeMessage(`keyword ${keyword} already exist in the list`);
+        snbCtx?.handleOpen();
+      }
+      return keyword.length > 0
+        ? (appCtx?.addOrderKeyword(keyword), setKeyword(""))
+        : false;
+    }
+  };
+
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    let action = (event.currentTarget as HTMLButtonElement).ariaLabel;
+
+    switch (action) {
+      case "template-settings":
+        dlgCtx?.handleView("My templates", <Template />);
+        dlgCtx?.handleOpen();
+        break;
+
+      case "order-submit":
+        break;
+
+      default:
+        break;
+    }
+  };
+
   return (
     <StyledOrderWrapper>
-      <Order />
+      <Box
+        sx={{
+          margin: "auto",
+          border: `solid 1px ${theme.colors.primary}`,
+          display: "flex",
+          flexWrap: "wrap",
+          position: "relative",
+          width: "80%",
+          minHeight: "70vh",
+          marginTop: "20px",
+        }}
+      >
+        <Record />
+        <Search
+          keyword={keyword}
+          changeKeyword={changeKeyword}
+          handleKeyDown={handleKeyDown}
+          handleClick={handleClick}
+        />
+      </Box>
     </StyledOrderWrapper>
   );
 };
--- src/components/OrderWrapper/StyledOrderWrapper.ts
+++ src/components/OrderWrapper/StyledOrderWrapper.ts
@@ -1,17 +1,27 @@
 import styled from "@emotion/styled";
 import { Box } from "@mui/material";
+import { theme } from "../../assets/themes";
 
 const StyledOrderWrapper = styled(Box)`
   min-height: 80vh;
   display: flex;
-  /* align-items: center;
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  justify-content: center;
-  margin: auto;
-  padding: 20px 0px;
-  width: 100%; */
+
+  & .order-title {
+    font-weight: bold;
+    color: ${theme.colors.primary};
+    padding: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 10vh;
+  }
+
+  & .order-content {
+    border: solid 1px ${theme.colors.primary};
+    height: 60vh;
+    padding: 10px;
+    border-radius: 5px;
+  }
 `;
 
 export default StyledOrderWrapper;
